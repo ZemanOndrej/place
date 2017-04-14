7e52765353508b30f2628b2edb9e1e32df243217
RayMan77 <ozeman77@gmail.com>
2017-04-14 11:05:55 +0200

    new: colorPicker; new: emit pixel handlers

--- public/js/place.js
+++ public/js/place.js
@@ -2,6 +2,9 @@
  * Created by zeman on 11-Apr-17.
  */
 (function () {
+    /**
+     * Init page
+     */
     "use strict";
     window.WebSocket = window.WebSocket || window.MozWebSocket;
     let name = false;
@@ -14,47 +17,40 @@
     let colorPicker = document.querySelector("#colorPicker");
     let socket = io('http://localhost:1337');
     let canvas = document.querySelector("canvas");
-    let pixSize =50;
+    let pixSize = 50;
     let board = [];
     let context = canvas.getContext('2d');
-    let mousePos = {x:0,y:0};
+    let mousePos = {x: 0, y: 0};
     const colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange', 'gray', 'black', 'white'];
     let selectedColor = false;
     overlayShade.width = window.innerWidth;
     overlayShade.height = window.innerHeight;
-
-
-    colors.forEach((color)=>{
+    colors.forEach((color) => {
         let newColor = document.createElement("div");
-        newColor.id=color+"ColorPicker";
+        newColor.id = color + "ColorPicker";
         newColor.classList.add(color);
         newColor.classList.add("color");
-        newColor.style.backgroundColor=color;
-        newColor.addEventListener("click",(event)=>
-        {
+        newColor.style.backgroundColor = color;
+        newColor.addEventListener("click", (event) => {
+            let div = colorPicker.querySelector("." + event.target.classList[0]);
 
-            console.log(event.target.classList[0]);
-            let div = colorPicker.querySelector("."+event.target.classList[0]);
-
-            if(selectedColor!==false){
-                let oldColor = colorPicker.querySelector("."+selectedColor);
-                oldColor.style.border="solid 2px black"
+            if (selectedColor !== false) {
+                let oldColor = colorPicker.querySelector("." + selectedColor);
+                oldColor.style.border = "solid 2px black"
 
             }
-            div.style.border=" white 2px solid";
+            div.style.border = " white 2px solid";
             selectedColor = event.target.classList[0];
 
         });
         colorPicker.appendChild(newColor);
 
 
-
     });
 
-
-
-
-
+    /**
+     *  Functions
+     */
 
     let getMousePos = function (evt) {
         let rect = canvas.getBoundingClientRect();
@@ -71,57 +67,7 @@
         context.fillText(message, 500, 20);
     };
 
-
-    submitName.addEventListener("click",()=>{
-        name = input.value;
-        input.setAttribute("disabled","disabled");
-        overlayShade.style.visibility="hidden";
-        overlay.style.visibility="hidden";
-        submitName.setAttribute("disabled","disabled");
-        socket.emit("start",{UserName:name});
-    });
-
-    canvas.addEventListener('mousemove', function(evt) {
-        mousePos = getMousePos( evt);
-    }, false);
-
-    socket.on("connected", (data)=> {
-        console.log("connected" );
-        // console.log(data);
-
-        board = data.board;
-        refreshCanvas();
-
-        overlay.style.visibility="visible";
-        overlayShade.style.visibility="visible";
-        input.removeAttribute("disabled");
-        submitName.removeAttribute("disabled");
-        status.innerHTML='Choose name:';
-    });
-
-    socket.on("error",  (error)=> {
-        console.log("error");
-        console.log(error);
-    });
-
-    socket.on("pixel",(data)=> {
-        // console.log(data);
-        let pixel = { pixelX:data.pixel.pixelX, pixelY:data.pixel.pixelY, author: data.pixel.author, time: data.pixel.time, pixelColor:data.pixel.pixelColor};
-        console.log(pixel);
-        board.push(pixel);
-
-    });
-
-
-    canvas.addEventListener('click', (event)=> {
-        if(selectedColor!==false){
-            socket.emit("pixel",{pixelColor:selectedColor,author:name,pixelX: Math.floor(event.clientX / 50), pixelY:Math.floor(event.clientY / 50)});
-            // console.log({pixelColor:selectedColor,author:name,pixelX:event.clientX/50, pixelY:event.clientY/50});
-        }
-    }, false);
-
-
-    function refreshCanvas() {
+    let refreshCanvas = function () {
         canvas.height = window.innerHeight;
         canvas.width = window.innerWidth;
 
@@ -130,12 +76,82 @@
         context.clearRect(0, 0, width, height);
         writeMessage('Mouse position: ' + mousePos.x + ',' + mousePos.y);
 
-        board.forEach((rec)=>{
-            context.fillStyle=rec.pixelColor;
-            context.fillRect(rec.pixelX* pixSize,rec.pixelY*pixSize,pixSize,pixSize);
+        board.forEach((rec) => {
+            context.fillStyle = rec.pixelColor;
+            context.fillRect(rec.pixelX * pixSize, rec.pixelY * pixSize, pixSize, pixSize);
         });
         window.requestAnimationFrame(refreshCanvas);
-    }
+    };
+
+
+    /**
+     * Event Listeners
+     */
+
+    submitName.addEventListener("click", () => {
+        name = input.value;
+        input.setAttribute("disabled", "disabled");
+        overlayShade.style.visibility = "hidden";
+        overlay.style.visibility = "hidden";
+        submitName.setAttribute("disabled", "disabled");
+    });
+
+    canvas.addEventListener('mousemove', function (evt) {
+        mousePos = getMousePos(evt);
+    }, false);
+
+    canvas.addEventListener('click', (event) => {
+        if (selectedColor !== false) {
+            socket.emit("pixel", {
+                pixelColor: selectedColor,
+                author: name,
+                pixelX: Math.floor(event.clientX / 50),
+                pixelY: Math.floor(event.clientY / 50)
+            });
+            // console.log({pixelColor:selectedColor,author:name,pixelX:event.clientX/50, pixelY:event.clientY/50});
+        }
+    }, false);
+
+    /**
+     * Socket listeners
+     */
+
+    socket.on("connected", (data) => {
+        console.log("connected");
+
+        board = data.board;
+
+        console.log(board);
+        overlay.style.visibility = "visible";
+        overlayShade.style.visibility = "visible";
+        input.removeAttribute("disabled");
+        submitName.removeAttribute("disabled");
+        status.innerHTML = 'Choose name:';
+    });
+
+    socket.on("error", (error) => {
+        console.log("error");
+        console.log(error);
+    });
+
+    socket.on("pixel", (data) => {
+        // console.log(data);
+        let pixel = {
+            pixelX: data.pixel.pixelX,
+            pixelY: data.pixel.pixelY,
+            author: data.pixel.author,
+            time: data.pixel.time,
+            pixelColor: data.pixel.pixelColor
+        };
+        console.log(pixel);
+        board.push(pixel);
+
+    });
+
+
+    /**
+     * Start
+     */
 
     refreshCanvas();
 
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 /**
  * Created by zeman on 11-Apr-17.
  */
+
+
+/**
+ * Server Init
+ */
 "use strict";
 process.title = 'node-place';
 
@@ -13,9 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-http.listen(port, "127.0.0.1",()=>console.log("app is listening on port:"+port));
-
-
+http.listen(port, "127.0.0.1", () => console.log("app is listening on port:" + port));
 
 
 /**
@@ -23,7 +26,7 @@ http.listen(port, "127.0.0.1",()=>console.log("app is listening on port:"+port))
  */
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/views/place.html'));
 
 });
@@ -34,89 +37,61 @@ app.get('/', function(req, res) {
 const colors = ['red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange', 'gray', 'black', 'white'];
 
 let board = [
-    {time: (new Date()).getTime(),
-    pixelX: 0,
-    pixelY:0,
-    author: "test",
-    pixelColor: colors[1]},
-    {time: (new Date()).getTime(),
+    {
+        time: (new Date()).getTime(),
+        pixelX: 0,
+        pixelY: 0,
+        author: "test",
+        pixelColor: colors[1]
+    },
+    {
+        time: (new Date()).getTime(),
         pixelX: 1,
-        pixelY:1,
+        pixelY: 1,
         author: "test2",
-        pixelColor: colors[0]},
+        pixelColor: colors[0]
+    },
 
-    {time: (new Date()).getTime(),
+    {
+        time: (new Date()).getTime(),
         pixelX: 4,
-        pixelY:5,
+        pixelY: 5,
         author: "test2",
-        pixelColor: colors[2]}
+        pixelColor: colors[2]
+    }
 
 ];
 
 
-
 /**
- * HTTP server
+ * WebSocket Listener
  */
-
-/**
- * WebSocket server
- */
-
-
-
 io.on('connection', function (socket) {
     console.log((new Date()) + ' Connection from origin ' + socket.request.connection.remoteAddress + '.');
-
-
-    let userName = false;
-    let pixelColor = false;
-    let pixelX = false;
-    let pixelY=false;
-
     console.log((new Date()) + ' Connection accepted.');
-    socket.emit("connected", {board:board});
+    socket.emit("connected", {board: board});
 
-
-    socket.on("start",(message)=>{
-        console.log(message.UserName);
-
-    });
-
-
-    socket.on('pixel',  (pixel)=> {
+    socket.on('pixel', (pixel) => {
         console.log((new Date()) + ' Received pixel from ' + pixel.author + ': ' + pixel);
-
-        // board.filter((x) => x.pixelX !== pixelX && x.pixelY !== pixelY);
-
-
+        // board = board.filter((x) => (x.pixelX !== pixel.pixelX) && (x.pixelY !== pixel.pixelY));
+        for(let i = 0;i<board.length;i++){
+            if(board[i].pixelY===pixel.pixelY && board[i].pixelX === pixel.pixelX){
+                board.remove(i);
+                break;
+            }
+        }
+        console.log(board.length);
         let obj = {
             time: (new Date()).getTime(),
             pixelX: pixel.pixelX,
-            pixelY:pixel.pixelY,
+            pixelY: pixel.pixelY,
             author: pixel.author,
-            pixelColor:pixel.pixelColor
+            pixelColor: pixel.pixelColor
         };
-        console.log(obj.pixelX);
-
         board.push(obj);
-
-        socket.emit("pixel",{pixel:obj});
-        // let json = JSON.stringify({type: 'message', data: obj});
-        //
-        // for (let i = 0; i < clients.length; i++) {
-        //     clients[i].sendUTF(json);
-        // }
-
-
+        socket.emit("pixel", {pixel: obj});
     });
 
     socket.on('disconnect', function (connection) {
-        if (userName !== false) {
-            console.log((new Date()) + " Peer "
-                + connection.remoteAddress + " disconnected.");
-            clients.splice(index, 1);
-        }
     });
-
 });
